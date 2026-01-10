337863bbcd63d9e4d1727a6f8748f3890459f166
Tonno Cramer <tonno@wxs.nl>
2026-01-10 12:38:08 +0100

    Add favicon and update browser title

--- vocatus/vocatus/urls.py
+++ vocatus/vocatus/urls.py
@@ -16,9 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.templatetags.static import static
+
+
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('bierguru/', include('bierguru.urls')),
+    path(
+    "favicon.ico",
+    RedirectView.as_view(url=static("favicon.ico")),
+  ),
     
 ]
